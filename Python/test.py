b8a43001679372ceea69dd3205bd57aeef40888f
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-02-21 22:56:29 +0530

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,7 @@
-print("hello")
+from py_flask import app
+
+def test_home():
+    # Test the home route
+    response = app.test_client().get("/")
+    
+    assert response.status_code==200  # Check for HTTP 200 status
